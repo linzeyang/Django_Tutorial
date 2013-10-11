42e02ebc5e986e365b1e73b28b10e7381246d73b
linzeyang <zeyanglin2013@gmail.com>
2013-10-11 19:09:39 +0100

    apply Django generic views

--- polls/urls.py
+++ polls/urls.py
@@ -1,16 +1,18 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 # Uncomment the next two lines to enable the admin:
 #from django.contrib import admin
 #admin.autodiscover()
 
-urlpatterns = patterns('polls.views',
+from polls import views
+
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
 
-    url(r'^$', 'index', name='index'),
-    url(r'^(?P<poll_id>\d+)/$', 'detail', name='detail'),
-    url(r'^(?P<poll_id>\d+)/results/$', 'results', name='results'),
-    url(r'^(?P<poll_id>\d+)/vote/$', 'vote', name='vote'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
--- polls/views.py
+++ polls/views.py
@@ -2,25 +2,28 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from polls.models import Poll, Choice
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    return render(request,
-                  'polls/index.html',
-                  {'latest_poll_list': latest_poll_list})
 
-    
-def detail(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': p})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
 
 
-def results(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll': p})
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
 
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
