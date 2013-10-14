62cad69c644455d5e5a40c2a858bf83330daaf8f
linzeyang <zeyanglin2013@gmail.com>
2013-10-14 11:08:17 +0100

    update settings.py: add two settings added in Django 1.5

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,6 +20,10 @@ DATABASES = {
     }
 }
 
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -125,6 +129,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'polls'
 )
+
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
