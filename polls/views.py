d3c61d72ab40098f283aae4155560d8b85cdb645
Zeyang Lin <zeyang@kubuntu20.04-i5-9600k>
2021-05-12 01:49:38 +0800

    Finishing tutorial part 2

---
+++ polls/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello world. You are at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at results of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
